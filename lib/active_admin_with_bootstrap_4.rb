d0ca36e51de39c70f548d25607dbccdc8c80f84f
Shashi Singh <shashi.we@gmail.com>
2019-06-29 21:42:19 +0530

    added engines

--- lib/active_admin_with_bootstrap_4.rb
+++ lib/active_admin_with_bootstrap_4.rb
@@ -1,5 +1,7 @@
 require "active_admin_with_bootstrap_4/version"
 
 module ActiveAdminWithBootstrap4
-  # Your code goes here...
+  class Error < StandardError; end
+  class Engine < ::Rails::Engine
+  end
 end
